e11fa03fe90e25ef6407b63bede1263aead1f111
Steven Taylor <steven.taylor@me.com>
2018-07-10 20:01:28 -0700

    revert

--- ecobee-api.js
+++ ecobee-api.js
@@ -392,7 +392,7 @@ function ecobeeApi() {
     };
 
     this.resumeProgram = ( id ) => {
-        return that.callFunction( id, 'resumeProgram', { resumeAll: true } );
+        return that.callFunction( id, 'resumeProgram', { resumeAll: false } );
     };
 
     this.getCurrent = () => {
